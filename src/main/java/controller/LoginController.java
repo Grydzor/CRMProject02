e6831691199cac0a9ae7bdbd1a3dd8abf06c8d84
Beregulya <beregulya.dev@gmail.com>
2017-02-05 17:27:34 +0200

    blalal

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,10 +1,15 @@
 package controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
 
@@ -17,8 +22,21 @@ public class LoginController {
     @FXML
     private Button btnEnter, btnExit;
 
-    public void enterButtonAction() {
-
+    public void enterButtonAction() throws IOException {
+        if (fldLogin.getText().equals("admin") && fldPassword.getText().equals("admin")){
+            Stage stage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("/view/admin_panel.fxml"));
+            stage.setTitle("Administration");
+            stage.setResizable(false);
+            stage.setScene(new Scene(root));
+            stage.show();
+            Stage old = (Stage) btnEnter.getScene().getWindow();
+            old.close();
+        } else {
+            fldLogin.setText("");
+            fldPassword.setText("");
+            fldPassword.setPromptText("PASSWORD IS WRONG!");
+        }
     }
 
     public void exitButtonAction() {
