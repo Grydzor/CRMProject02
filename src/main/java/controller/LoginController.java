fc66289cd0c278434e46beef79608babceb8f6d5
Aleksey <aleksey.mykhalsky@gmail.com>
2017-02-05 16:43:23 +0200

    controllers

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,5 +1,22 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+
+    @FXML
+    private TextField fldLogin;
+
+    @FXML
+    private PasswordField fldPassword;
+
+    @FXML
+    private Button btnEnter, btnExit;
+
+
+
 
 }
