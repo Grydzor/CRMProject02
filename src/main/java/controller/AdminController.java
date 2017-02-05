3eb54260fc40080b5965ff1f57d126a25cc83098
Beregulya <beregulya.dev@gmail.com>
2017-02-05 17:46:56 +0200

    blalal

--- src/main/java/controller/AdminController.java
+++ src/main/java/controller/AdminController.java
@@ -1,8 +1,14 @@
 package controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AdminController {
 
@@ -12,6 +18,15 @@ public class AdminController {
     @FXML
     private Button btnCreate, btnDelete, btnChange, btnGenerate;
 
-
+    public void createButtonAction() throws IOException {
+        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/view/create_panel.fxml"));
+        stage.setTitle("Create...");
+        stage.setResizable(false);
+        stage.setScene(new Scene(root));
+        stage.show();
+        Stage old = (Stage) btnCreate.getScene().getWindow();
+        old.close();
+    }
 
 }
