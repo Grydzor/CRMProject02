ad419d261f246570d73ccfc2275a46d70fc29dea
Beregulya <beregulya.dev@gmail.com>
2017-02-05 15:57:56 +0200

    cfg + session

--- src/main/java/util/HibernateSessionFactory.java
+++ src/main/java/util/HibernateSessionFactory.java
@@ -1,5 +1,31 @@
 package util;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 public class HibernateSessionFactory {
+
+    private static SessionFactory sessionFactory = buildSessionFactory();
+
+    protected static SessionFactory buildSessionFactory() {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        try {
+            sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+        } catch (Exception e) {
+            StandardServiceRegistryBuilder.destroy(registry);
+            throw new ExceptionInInitializerError("Initial SessionFactory failed" + e);
+        }
+        return sessionFactory;
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutDown() {
+        getSessionFactory().close();
+    }
 
 }
